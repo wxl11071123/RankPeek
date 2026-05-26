2f2295de56bcc4756305f114363c1411f3196634
wxl11071123 <wxl11071123@users.noreply.github.com>
2026-05-26 14:03:50 +0800

    Stabilize async DeepSeek stream test

--- rankpeek-server/src/test/java/io/rankpeek/server/analysis/DeepSeekAnalysisControllerTest.java
+++ rankpeek-server/src/test/java/io/rankpeek/server/analysis/DeepSeekAnalysisControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -48,7 +49,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
 @ActiveProfiles("test")
 @DirtiesContext
 class DeepSeekAnalysisControllerTest {
